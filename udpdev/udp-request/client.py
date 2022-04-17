f2e2b2930a0921c0a89a17c470c22de80619f054
Sabrina <34215580+BwinaRose@users.noreply.github.com>
2022-04-17 19:30:51 -0400

    Update client.py

--- udpdev/udp-request/client.py
+++ udpdev/udp-request/client.py
@@ -1,27 +1,30 @@
-
 import socket
-import pickle
-
 
 def client_program():
     host = socket.gethostname() 
-    port = 50080
+    port = 50097
 
     client_socket = socket.socket() 
     client_socket.connect((host, port)) 
     print("type 'quit' to close connection")
+
     message = input(" -> ") 
+    data = None
 
     while message.lower().strip() != 'quit':
         message = (message).encode()
         client_socket.send(message)
-        data = client_socket.recv(1024)
-        data = pickle.loads(data)
-
-        print(data) 
-
+        # data = client_socket.recv(1024).decode('utf8')
+        # data = pickle.loads(data)
+        # print(data) 
+        print("HI")
+        while data != "DONE":
+            data = client_socket.recv(1024).decode('utf8')
+            print(data)
+        print("HEY TOO")
         message = input(" -> ") 
 
+    print("GOODBYE")
     client_socket.close() 
 
 
